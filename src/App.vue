dee19a0c7a4cd0aeb852d9c5bf999da0db0d85b2
dezhizhang <1541609448@qq,com>
2018-07-26 22:54:39 +0800

    功能的实现

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,15 @@
     <form role='form'>
        <div class='form-group'>
          <label from='username'>用户名</label>
-         <input type='text' class='form-control' placeholder='请输入用户名'/>
+         <input type='text' class='form-control' placeholder='请输入用户名' v-model='username'/>
        </div>
        <div class='form-group'>
          <label from='userage'>年 龄</label>
-         <input type='text' class='form-control' placeholder='请输入年龄'/>
+         <input type='text' class='form-control' placeholder='请输入年龄' v-model='userage'/>
        </div>
        <div class='form-group'>
-          <input type='button' class='btn btn-primary' value='添加'/>
-          <input type='button' class='btn btn-danger' value='重置'/>
+          <input type='button' class='btn btn-primary' value='添加' @click='add'/>
+          <input type='button' class='btn btn-danger' value='重置' @click='reset'/>
        </div>
     </form>
     <hr/>
@@ -24,41 +24,31 @@
          <th>年龄</th>
          <th>操作</th>
        </tr>
-       <tr class='text-center'>
-         <td>1</td>
-         <td>张德志</td>
-         <td>28</td>
+       <tr class='text-center' v-for='(item,index) in myData' :key='item.id'>
+         <td>{{index+1}}</td>
+         <td>{{item.name}}</td>
+         <td>{{item.age}}</td>
          <td>
-           <button class='btn btn-danger btn-sm'>删除</button>
+           <button class='btn btn-danger btn-sm' @click='fnDelete(index)'>删除</button>
          </td>
        </tr>
-       <tr class='text-center'>
-         <td>2</td>
-         <td>刘德华</td>
-         <td>22</td>
-         <td>
-           <button class='btn btn-danger btn-sm'>删除</button>
-         </td>
-       </tr>
-       <tr class='text-center'>
-         <td>3</td>
-         <td> 周华建</td>
-         <td>33</td>
-         <td>
-           <button class='btn btn-danger btn-sm'>删除</button>
-         </td>
-       </tr>
-       <tr>
+       
+       <tr v-show='myData.length!=0'>
           <td class='text-right' colspan='4'>
-             <button class='btn btn-danger btn-sm'>全部删除</button>
+             <button class='btn btn-danger btn-sm' @click='fnComplete'>全部删除</button>
           </td>
        </tr>
-       <tr class='text-center'>
+       <tr class='text-center' v-show='myData.length==0'>
          <td colspan='4'>
            <p>输无数据......</p>
          </td>
        </tr>
      </table>
+
+     <!--模态框-->
+     <div role='dialog' class='modal' id='layer'>
+     
+     </div>
   </div>
 </template>
 
@@ -69,6 +59,48 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      myData:[
+        
+      ],
+      username:'',
+      userage:''
+    }
+  },
+  methods:{
+    add(){
+    if(this.username!='' && this.userage!=''){
+      this.myData.push({
+        name:this.username,
+        age:this.userage
+      }),
+      this.username='';
+      this.userage='';
+
+
+      }else{
+        alert('内容不能为空!');
+
+      }
+    },
+    reset(){
+      this.username='';
+      this.userage='';
+
+    },
+    fnDelete(index){
+      this.myData.splice(index,1);
+
+
+    },
+
+    fnComplete(){
+      this.myData=[]
+
+
+    }
   }
 }
 </script>
